5a7b47ba31255b8e6b2f15a9608416266de9a8ad
corinthians23 <165019098+corinthians23@users.noreply.github.com>
2024-04-01 12:05:06 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Giraffe)
+    basic.pause(100)
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . # . . .
+        . # # # #
+        . # . . #
+        `)
+})
